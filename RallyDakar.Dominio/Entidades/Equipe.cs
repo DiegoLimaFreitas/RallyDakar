3a7cc071c151fbeb30e72a07ed366ef60c19937e
Diego Lima de Freitas <diegomarcandalli2@gmail.com>
2021-06-15 16:30:30 -0300

    Primeira versão

---
+++ RallyDakar.Dominio/Entidades/Equipe.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RallyDakar.Dominio.Entidades
+{
+    public class Equipe
+    {
+
+        public int Id { get; set; }
+        public string CodigoIdentificador { get; set; }
+        public string Nome { get; set; }
+
+        public int TemporadaId { get; set; }
+
+        //Quando o sistema estiver em execução o entity framework pode carregar por demanda, mudando a instancia em tempo de execução
+        public virtual Temporada Temporada { get; set; }
+
+        public ICollection<Piloto> Pilotos { get; set; }
+    }
+}
---
+++ RallyDakar.Dominio/Entidades/Piloto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RallyDakar.Dominio.Entidades
+{
+   public class Piloto
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public int EquipeId { get; set; }
+                
+        public virtual ICollection<Equipe> Equipes { get; set; }
+    }
+}
